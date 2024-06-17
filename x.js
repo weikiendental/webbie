d5ff45563d24f69bc0e33da5af1117812b953d6c
stickfigure227 <stickfigure227@gmail.com>
2024-06-17 18:05:12 +0800

    Add files via upload

---
+++ x.js
@@ -0,0 +1,29 @@
+var bg = document.getElementById("bg");
+var fg = document.getElementById("fg");
+var longButton = document.getElementById("longButton");
+var span = document.getElementById("closeBtn");
+
+longButton.onclick = function () {
+  bg.style.display = "none";
+  fg.style.display = "flex";
+}
+
+span.onclick = function () {
+  bg.style.display = "flex";
+  fg.style.display = "none";
+}
+
+var KPTee = document.getElementById("KPTee");
+var KPTeecare = document.getElementById("KPTeecare");
+var KPTeeButton = document.getElementById("KPTeeButton");
+var KPTeecareButton = document.getElementById("KPTeecareButton");
+
+KPTeeButton.onclick = function () {
+  KPTee.style.display = "flex";
+  KPTeecare.style.display = "none";
+}
+
+KPTeecareButton.onclick = function () {
+  KPTeecare.style.display = "flex";
+  KPTee.style.display = "none";
+}
